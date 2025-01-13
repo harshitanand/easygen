76e8a760fdcf2a47eb3f766dd5ea0686b84ea3f5
Harshit Anand <harshitanand94@gmail.com>
2025-01-13 06:19:25 +0530

    Redirect to signup page on home component load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  redirect('/signup'); // Redirect to the signup page
+  return null;
 }
